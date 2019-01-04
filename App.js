4f808a3ad691f1fb5b11dc508033dfe2255d2ab4
CesarRivasP <cesarjose58@gmail.com>
2019-01-03 22:29:47 -0400

    implementation of component Header and SuggestionList in appJS

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import { Text } from 'react-native';
 import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list'
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
-        <Text>Header</Text>
+        <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
-        <Text>Sugerencias</Text>
+        <SuggestionList />
       </Home>
     );
   }
