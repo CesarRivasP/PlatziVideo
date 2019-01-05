7d5f9a789a357057f7bb3485fdc1296ded6c24ca
CesarRivasP <cesarjose58@gmail.com>
2019-01-04 23:26:17 -0400

    Adding component empty

---
+++ src/videos/components/empty.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Empty = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text styles={styles.text}>{ props.text }</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+  }
+})
+export default Empty;
