7f4edaebcec1bce42881eb0c3f9cc89bc0428da2
CesarRivasP <cesarjose58@gmail.com>
2019-01-06 14:00:50 -0400

    Added componentDidMount with api query

--- App.js
+++ App.js
@@ -4,15 +4,30 @@ import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list'
 
+import API from './utils/api'
+
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    suggestionList: []
+  }
+  // componentDidMount(){
+  // Para qe el componentDidMount sea asincrono
+  async componentDidMount() {
+    //Aqui se va a mandar la peticion a la api
+    // API.getSuggestion(10);
+    const movies = await API.getSuggestion(10);
+    console.log(movies);
+    this.setState({ suggestionList: movies })
+  }
+
   render() {
     return (
       <Home>
         <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
-        <SuggestionList />
+        <SuggestionList list={this.state.suggestionList}/>
       </Home>
     );
   }
