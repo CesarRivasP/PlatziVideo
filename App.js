8a770a6a53d45669cc7d5a38fc572df5c1664476
CesarRivasP <cesarjose58@gmail.com>
2019-01-09 22:43:30 -0400

    Delele tag view that contained to player component

--- App.js
+++ App.js
@@ -35,9 +35,7 @@ export default class App extends Component<Props> {
     return (
       <Home>
         <Header />
-        <View style={styles.containerVideo}>
-          <Player />
-        </View>
+        <Player />
         <Text>Buscador</Text>
         {/* {
           loading ?
