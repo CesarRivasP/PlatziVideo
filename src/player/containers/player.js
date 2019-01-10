3a41e22e71aa958c5dc219aaacbce5e62c1d9929
CesarRivasP <cesarjose58@gmail.com>
2019-01-09 20:14:05 -0400

    Added activityIndicator and loader props

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import Layout from '../components/layout.js';
 import Video from 'react-native-video';
 
 class Player extends Component {
+  state = {
+    loading: true,
+  }
+  onBuffer = ({isBuffering}) => {  // isBuffering indica si esta cargando o no
+    this.setState({ loading: isBuffering })
+  }
+
+  // onLoad = () => {
+  //   this.setState({ loading: false }) mediante onLoad indica cuando ya cargo el video
+  // }
+
   render() {
+    const { loading } = this.state;
     return (
       <Layout
+        loading={loading}
         video={ //asi se envia un componente como si fuera una propiedad
           <Video
             source={{ uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
             style={styles.video}  //asi se hace re size en si mismo
             resizeMode="contain" //Asi mantiene sus proporciones | cover aprovecha todo el tamaño
+            onBuffer={this.onBuffer}
+            // onLoad={this.onLoad} en caso de que no funcione en android la validacion con onBuffering
           />
+        }
+        loader={
+          <ActivityIndicator color="red" />
         }>
       </Layout>
     );
@@ -36,4 +54,5 @@ un tamaño definido para que el video haga resize y se ponga en posicion absolut
 - De la forma en la que se esta pasando el componente al layout, indica que en el layout, un
 componente que aun no se ha creado, le va a llegar una propiedad video la cual va a contener
 el componente video
+Si una propiedad esta en {true|false} significa que esta interactuando
 */
