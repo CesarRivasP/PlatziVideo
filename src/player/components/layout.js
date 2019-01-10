7031c4c7fe493e7fa32c66ae4f2336b64dfec842
CesarRivasP <cesarjose58@gmail.com>
2019-01-09 20:13:32 -0400

    Adding loader components with functions

--- src/player/components/layout.js
+++ src/player/components/layout.js
@@ -3,10 +3,17 @@ import { View, Text, StyleSheet } from 'react-native';
 
 
 const Layout = (props) => {
+  const { video, loader, loading } = props;
   return (
     <View style={styles.container}>
       <View style={styles.video}>
-        { props.video }
+        { video }
+      </View>
+      <View style={styles.overlay}>
+        {
+          loading &&
+            loader
+        }
       </View>
     </View>
   );
@@ -24,6 +31,15 @@ const styles = StyleSheet.create({
     bottom: 0,
     backgroundColor: 'black'
   },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
 export default Layout;
 
