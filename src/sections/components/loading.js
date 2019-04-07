a4932f9cf6b81c5cd29bc40fbe5fa056ed23f235
CesarRivasP <cesarjose58@gmail.com>
2019-04-07 18:18:50 -0400

    Changes in styles

--- src/sections/components/loading.js
+++ src/sections/components/loading.js
@@ -2,22 +2,31 @@ import React from 'react';
 import {
   View,
   ActivityIndicator,
+  Image,
   StyleSheet
 } from 'react-native';
 
 
 const Loading = (props) => {
   return (
-    <View style={styles.containerLoading}>
+    <View style={styles.container}>
+      <Image source={require('../../../assets/logo.png')} style={styles.logo}/>
       <ActivityIndicator size="large" color="#0000ff" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  containerLoading: {
-  flex: 1,
-  justifyContent: 'center'
+  container: {
+    flex: 1,
+    justifyContent: 'center', //en lo vertical
+    alignItems: 'center'  //en lo horizontal
+  },
+  logo: {
+    marginBottom: 10,
+    width: 200,
+    height: 80,
+    resizeMode: 'contain'
   }
 })
 
