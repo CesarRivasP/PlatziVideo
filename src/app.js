b9e03f00760621c0502f28bd616b1f4ee1c0d8eb
CesarRivasP <cesarjose58@gmail.com>
2019-04-08 21:10:00 -0400

    Added and implemented movie component with conditions

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import Home from './screens/containers/home';
 import SuggestionList from './videos/containers/suggestion-list';
 import CategoryList from './videos/containers/category-list';
-import Player from './player/containers/player';
 import Header from './sections/components/header';
 import API from '../utils/api';
+import Movie from './screens/containers/movie';
+
 
 class AppLayout extends Component {
 
@@ -38,10 +39,15 @@ class AppLayout extends Component {
   }
 
   render () {
+
+    if(this.props.selectedMovie) {
+      return <Movie />
+    }
+
     return (
       <Home>
         <Header />
-        <Player />
+        {/* <Player /> */}
         <Text>Buscador</Text>
         {/* {
           loading ?
@@ -54,4 +60,20 @@ class AppLayout extends Component {
   }
 }
 
-export default connect(null)(AppLayout);
+// const styles = StyleSheet.create({
+//   containerLoading: {
+//     flex: 1,
+//     justifyContent: 'center'
+//   },
+//   containerVideo: {
+//      flex: 1,
+//      height: 300
+//   }
+// })
+
+const mapStateToProps = (state) => {
+  return {
+    selectedMovie: state.selectedMovie
+  }
+}
+export default connect(mapStateToProps)(AppLayout);
