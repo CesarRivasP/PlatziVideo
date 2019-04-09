cb24e9b4dc45d1a6582fb09e6f0b26973df33b60
CesarRivasP <cesarjose58@gmail.com>
2019-04-09 18:36:27 -0400

    Added makehtml function and webview with youtube trailer

--- src/videos/components/details.js
+++ src/videos/components/details.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View,
+  Text,
+  Image,
+  StyleSheet,
+  WebView,
+  ScrollView
+ } from 'react-native';
+
+const makeHtml = (id) => {
+  return (
+    ` <style>
+        .video {
+          position: relative;
+          padding-bottom: 50%;
+        }
+        iframe {
+          position: absolute;
+          left: 0;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          width: 100%;
+          height: 100%;
+        }
+      </style>
+      <div className="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/${id}"
+          frameborder="0"
+          allow="accelerometer;
+          autoplay;
+          encrypted-media;
+          gyroscope;
+          picture-in-picture"
+          allowfullscreen>
+        </iframe>
+      </div>
+    `
+  );
+}
 
 
 const Details = (props) => {
   return (
-    <View>
+    <ScrollView>
       <View style={styles.top}>
         <Text>{ props.title }</Text>
       </View>
@@ -14,7 +55,10 @@ const Details = (props) => {
           <Text style={styles.description}>{ props.description_full }</Text>
         </View>
       </View>
-    </View>
+      <View style={styles.trailer}>
+        <WebView source={{html: makeHtml(props.yt_trailer_code) }}/>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -43,6 +87,10 @@ const styles = StyleSheet.create({
     color: '#4c4c4c',
     marginLeft: 10,
     flex: 1
+  },
+  trailer: {
+    height: 200,
+    marginBottom: 20
   }
 })
 export default Details;
