0b38df01e06fe61a1d9ec6d48fbb5e82fa7d8e60
CesarRivasP <cesarjose58@gmail.com>
2019-04-09 17:32:49 -0400

    Added closeVideo Function and implemented redux connect

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import MovieLayout from '../components/movie-layout';
 import Header from '../../sections/components/header';
 import Player from '../../player/containers/player';
+import Close from '../../sections/components/close';
 
 
 class Movie extends Component {
+
+  closeVideo = () => {
+    this.props.dispatch({
+      type: 'SET_SELECTED_MOVIE',
+      payload: {
+        movie: null
+      }
+    });
+  }
+
   render () {
     return (
       <MovieLayout>
-        <Header />
+        <Header>
+          <Close onPress={ this.closeVideo } />
+        </Header>
         <Player />
       </MovieLayout>
     );
   }
 }
 
-export default Movie;
+export default connect(null)(Movie);
