316c90ac8d38df6fe8068fab1f9699902e31062a
CesarRivasP <cesarjose58@gmail.com>
2019-04-09 21:12:52 -0400

    Implemented animations

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Animated } from 'react-native';
 import { connect } from 'react-redux';
 import MovieLayout from '../components/movie-layout';
 import Header from '../../sections/components/header';
@@ -8,6 +9,9 @@ import Details from '../../videos/components/details';
 
 
 class Movie extends Component {
+  state = {
+    opacity: new Animated.Value(0),
+  }
 
   closeVideo = () => {
     this.props.dispatch({
@@ -18,15 +22,32 @@ class Movie extends Component {
     });
   }
 
+  componentDidMount() {
+    Animated.timing(
+      // recibe que valor y en que tiempo
+      this.state.opacity,
+      { //0 es transparente y 1 es totalmente visible
+        toValue: 1, // para que pase a 1
+        duration: 1000,//cuanto tiempo se quiere que demore
+        // por defecto son 500 milisegundos
+      }
+    ).start();
+  }
+
   render () {
     return (
-      <MovieLayout>
-        <Header>
-          <Close onPress={ this.closeVideo } />
-        </Header>
-        <Player />
-        <Details { ...this.props.movie }/>
-      </MovieLayout>
+      <Animated.View style={{
+        flex: 1,  //para que ocupe todo el espacio
+        opacity: this.state.opacity  //puede ir desde 0 a 1
+      }}>
+        <MovieLayout>
+          <Header>
+            <Close onPress={ this.closeVideo } />
+          </Header>
+          <Player />
+          <Details { ...this.props.movie }/>
+        </MovieLayout>
+      </Animated.View>
     );
   }
 }
