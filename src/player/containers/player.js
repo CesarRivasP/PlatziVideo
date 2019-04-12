5ed5b0490a0b24b77b79c27527e3913cbf78d2b6
CesarRivasP <cesarjose58@gmail.com>
2019-04-11 21:53:51 -0400

    Added controls of duration

--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableWithoutFeedback,
+  ProgressBarAndroid
+ } from 'react-native';
 import Video from 'react-native-video';
+import { connect } from 'react-redux';
 import Layout from '../components/layout';
 import ControlLayout from '../components/control-layout';
 import PlayPause from '../components/play-pause';
@@ -11,11 +19,57 @@ class Player extends Component {
   state = {
     loading: true,
     paused: false,
-    fullscreen: false
+    fullscreen: false,
+    muted: false,
+    // currentTime: 0.0,
+    // duration: 0.0,
+    volume: 1,
+    repeat: false,
+    rate: 1,
+    pausedText: 'PLAY'
   }
 
   onBuffer = ({isBuffering}) => {  // isBuffering indica si esta cargando o no
     this.setState({ loading: isBuffering })
+  }
+
+  setTime = (payload) => {
+    let currentTime= timeReadable(payload.currentTime);
+    this.props.dispatch({
+      type: 'UPDATE_PROGRESS_BAR',
+      payload: {
+        currentTime,
+        progress: (payload.currentTime / payload.seekableDuration),
+        duration: payload.seekableDuration
+      }
+    })
+  }
+
+  changeSliderStarted = (value) => {
+    this.props.dispatch({
+      type: 'CHANGE_SLIDER_STARTED',
+      payload: {
+        progress: value,
+        currentTime: timeReadable(this.props.duration * value),
+        changeActive: true
+      }
+    })
+  };
+
+  changeSliderFinished = (value) => {
+    this.props.dispatch({
+      type: 'CHANGE_SLIDER_FINISHED',
+      payload: {
+        changeActive: false
+      }
+    })
+    this.props.seek(this.props.duration * value;)
+  };
+
+
+  onEnd = () => {
+    this.setState({ pausedText: 'PLAY', paused: true })
+    this.video.seek(0);
   }
 
   // onLoad = () => {
@@ -40,15 +94,25 @@ class Player extends Component {
       <Layout
         loading={loading}
         video={ //asi se envia un componente como si fuera una propiedad
-          <Video
-            source={{ uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
-            style={styles.video}  //asi se hace re size en si mismo
-            resizeMode="contain" //Asi mantiene sus proporciones | cover aprovecha todo el tamaño
-            onBuffer={this.onBuffer}
-            // onLoad={this.onLoad} en caso de que no funcione en android la validacion con onBuffering
-            paused={this.state.paused}
-            fullscreen={this.state.fullscreen}
-          />
+          <TouchableWithoutFeedback onPress={() => this.setState({paused: !this.state.paused})}>
+            <Video
+              source={{ uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'}}
+              style={styles.video}  //asi se hace re size en si mismo
+              resizeMode="contain" //Asi mantiene sus proporciones | cover aprovecha todo el tamaño
+              onBuffer={this.onBuffer}
+              // onLoad={this.onLoad} en caso de que no funcione en android la validacion con onBuffering
+              paused={this.state.paused}
+              fullscreen={this.state.fullscreen}
+              onEnd={this.onEnd}
+              onProgress={this.setTime}
+              volume={this.state.volume}
+              muted={this.state.muted}
+              repeat={this.state.repeat}
+              rate={this.state.rate}
+              hideConstrols={false}
+            />
+          </TouchableWithoutFeedback>
+
         }
         loader={
           <ActivityIndicator color="red" />
@@ -56,11 +120,11 @@ class Player extends Component {
         controls={
           <ControlLayout>
             <PlayPause onPress={this.playPause} paused={this.state.paused}/>
-            {/* <ProgressBar 
+            <ProgressBar
               progress={this.props.progress}
-              onChangeStarted={this.changeSliderStarted}
-              onChangeFinished={this.changeSliderFinished}
-            /> */}
+              onChangeStarted={this.props.changeSliderStarted}
+              onChangeFinished={this.props.changeSliderFinished}
+            />
             <Text>time left</Text>
             <FullScreen onPress={this.fullScreen} fullscreen={this.state.fullscreen}/>
           </ControlLayout>
@@ -68,6 +132,15 @@ class Player extends Component {
       </Layout>
     );
   }
+}
+const timeReadable = (time)  => {
+  let duration = time / 60;
+  let mins = Math.floor(duration);
+  let seconds = duration % 1;
+  seconds = (seconds * 60) / 1000;
+  let currentTime = (mins + seconds * 10).toFixed(2);
+  
+  return currentTime;
 }
 
 const styles = StyleSheet.create({
@@ -79,7 +152,17 @@ const styles = StyleSheet.create({
     top: 0
   }
 })
-export default Player;
+
+const mapStateToProps = (state) => {
+  return {
+    progress: state.progress,
+    currentTime: state.currentTime,
+    duration: state.duration,
+    changeActive: state.changeActive,
+  }
+}
+
+export default connect(mapStateToProps)(Player);
 
 /*
 Con todo esto el video va a ser flexible, es decir, que su layout es el que va a necesitar
