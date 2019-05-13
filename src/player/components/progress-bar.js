a7445e23b812b47b6bb5b93d56f96a195622299d
CesarRivasP <cesarjose58@gmail.com>
2019-05-13 18:26:21 -0400

    Create and implemented ProgressBar component

--- src/player/components/progress-bar.js
+++ src/player/components/progress-bar.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { View, Text, Slider } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import BarProgress from 'react-native-progress/Bar';
 
-const ProgressBar = (props) => {
-  return (
-    <View>
-      <Slider
-        maximumValue={props.duration}
-        minimumValue={0}
+class ProgressBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      duration: props.duration
+    }
+  }
 
-      />
-    </View>
-  );
+  handleProgressPress = (e) => {
+    const position = e.nativeEvent.locationX;
+    const progress = (position / 250) * this.state.duration;
+    this.props.player.seek(progress)
+  };
+
+  render(){
+    return (
+      <View>
+        <TouchableWithoutFeedback onPress={this.handleProgressPress}>
+          <View>
+            <BarProgress
+              progress={this.props.progress}
+              color="#FFF"
+              unfilledColor="rgba(255 , 255, 255, .5)"
+              borderColor="#FFF"
+              width={250}
+              height={20}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
 }
 
 export default ProgressBar;
